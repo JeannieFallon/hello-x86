3ba70c3ba372b48243261a2753ce0b3a5043c4ac
jeannie <jeanniefallon@gmail.com>
2019-05-05 11:46:22 -0700

    translate tabs to 2 spaces

--- 01_uno/uno.asm
+++ 01_uno/uno.asm
@@ -1,7 +1,7 @@
-global _start			; define entry point using label
+global _start           ; define entry point using label
 _start:
 
-	mov eax, 1			; move int value 1 into register eax
-	mov ebx, 42			; move int value 42 into register ebx
-	int 0x80			  ; exit
+    mov eax, 1          ; move int value 1 into register eax
+    mov ebx, 42         ; move int value 42 into register ebx
+    int 0x80            ; exit
 	
