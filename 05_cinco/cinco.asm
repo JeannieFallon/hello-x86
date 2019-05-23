2169f095e87677935a1489ed917f3d7b2d81b01c
jeannie <jeanniefallon@gmail.com>
2019-05-23 15:15:43 -0700

    basic stack operations in cinco

--- 05_cinco/cinco.asm
+++ 05_cinco/cinco.asm
@@ -1,15 +1,18 @@
 global _start:
 
 _start:
-  ; TODO
-  
+  sub esp, 4            ; allocate 4 bytes on stack (move pointer down)
+  mov [esp], byte 'H'   ; move byte 'H' into first space allocated
+  mov [esp+1], byte 'e' ; move byte 'e' into offset space (+1)
+  mov [esp+2], byte 'y' ; repeat, offset +2
+  mov [esp+3], byte '!' ; repeat, offset +3
   ; sys_write, stdout, and bytes to write
-  mov eax, 4
-  mov ebx, 1
-  mov ecx, esp
-  mov edx, 4
-  int 0x80
+  mov eax, 4            ; 4 is sys_write
+  mov ebx, 1            ; 1 is stdout file descript
+  mov ecx, esp          ; set ecx to address that esp points to
+  mov edx, 4            ; number of bytes to write
+  int 0x80              ; perform system call
   ; sys_exit, exit status
-  mov eax, 1
-  mov ebx, 0
-  int 0x80
+  mov eax, 1            ; 1 is sys_exit
+  mov ebx, 0            ; exit status is 0 (success)
+  int 0x80              ; perform system call
