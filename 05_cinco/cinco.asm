358ab599f7f0b87d51f3d29894979fd94f0a88cc
jeannie <jeanniefallon@gmail.com>
2019-05-23 15:19:33 -0700

    add newline to output

--- 05_cinco/cinco.asm
+++ 05_cinco/cinco.asm
@@ -6,11 +6,12 @@ _start:
   mov [esp+1], byte 'e' ; move byte 'e' into offset space (+1)
   mov [esp+2], byte 'y' ; repeat, offset +2
   mov [esp+3], byte '!' ; repeat, offset +3
+  mov [esp+4], byte 0x0a  ; newline
   ; sys_write, stdout, and bytes to write
   mov eax, 4            ; 4 is sys_write
   mov ebx, 1            ; 1 is stdout file descript
   mov ecx, esp          ; set ecx to address that esp points to
-  mov edx, 4            ; number of bytes to write
+  mov edx, 5            ; number of bytes to write
   int 0x80              ; perform system call
   ; sys_exit, exit status
   mov eax, 1            ; 1 is sys_exit
