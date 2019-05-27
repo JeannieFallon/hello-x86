a6ac1f116cbbc0a190928a55cce17e15bd313ed8
jeannie <jeanniefallon@gmail.com>
2019-05-27 16:46:26 -0700

    add newline to output

--- 07_siete/siete_basic.asm
+++ 07_siete/siete_basic.asm
@@ -8,14 +8,15 @@ _start:
 
 greeting:
   mov ebp, esp        ; move value of esp into ebp (store top of stack)
-  sub esp, 2          ; subtract 2 from esp (allocate 2 bytes on stack)
+  sub esp, 3          ; subtract 3 from esp (allocate 3 bytes on stack)
   mov [esp], byte 'H' ; move 'H' into 1st allocated byte
-  mov [esp+1], byte 'i' ; move 'i' into 2nd allcated byte
-  ; perform standard output
+  mov [esp+1], byte 'i' ; move 'i' into 2nd allocated byte
+  mov [esp+2], byte 0x0a  ; move newline into 3rd allocated byte
+  ; standard output code
   mov eax, 4          ; 4 is sys_write
   mov ebx, 1          ; 1 is stdout file descriptor
   mov ecx, esp        ; move bytes to write into ecx
-  mov edx, 2          ; number of bytes to write
+  mov edx, 3          ; number of bytes to write
   int 0x80            ; perform system call (set to sys_write)
   ; restore stack & return back to previous location of execution
   mov esp, ebp        ; move previously stored value back into esp
